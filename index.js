57c0fa746850029c5c9faf84802831f4970332cb
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-09-28 12:31:14 +0200

    Express integration

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Hello world')
+const express = require('express')
+const app = express()
+
+let players = [
+    {
+        id: 1,
+        playerName: 'Lionel Messi',
+        nation: 'Argentina',
+        position: 'RW',
+        team: 'Inter Miami (CAL)',
+        rating: 99
+    },
+    {
+        id: 2,
+        playerName: 'Cristiano Ronaldo',
+        nation: 'Portugal',
+        position: 'ST',
+        team: 'Al Nassr (ARA)',
+        rating: 95
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Players Ratings</h1>')
+})
+
+app.get('/api/players', (req, res) => {
+    res.json(players)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
