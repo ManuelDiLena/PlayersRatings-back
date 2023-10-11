120f185fef5dd1dfe9ef3469c026c64f753ac28a
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-10-11 17:32:04 +0200

    Creating users

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
-const playersRouter = require('./controllers/players')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
+const playersRouter = require('./controllers/players')
+const usersRouter = require('./controllers/users')
 
 logger.info('Connecting to', config.MONGODB_URI)
 
@@ -23,6 +25,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/players', playersRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
