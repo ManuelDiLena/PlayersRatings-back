2127bbb66b5e6a1aa199bbbf3f12df17cd2b4f33
ManuelDiLena <manuel.dilena.29@gmail.com>
2023-10-12 19:48:56 +0200

    Token validity period

--- controllers/login.js
+++ controllers/login.js
@@ -23,7 +23,12 @@ loginRouter.post('/', async (req, res) => {
         id: user._id,
     }
 
-    const token = jwt.sign(userForToken, process.env.SECRET)
+    // Token expires in 60*60 seconds, that is, in one hour
+    const token = jwt.sign(
+        userForToken,
+        process.env.SECRET,
+        { expiresIn: 60*60 }
+    )
 
     res.status(200)
         .send({ token, username: user.username, name: user.name })
